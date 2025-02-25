469831a0ac8fbc739e02915eaa592e24973aba26
Yohann DANA <yodana@paul-f4Br2s2.clusters.42paris.fr>
2025-02-25 18:18:11 +0100

    Day03

--- Day03/ex00/S1E9.py
+++ Day03/ex00/S1E9.py
@@ -9,14 +9,17 @@ class Character(ABC):
         """Init the name and is alive"""
         self.name = name
         self.is_alive = is_alive
-
+    
+    @abstractmethod
     def die(self):
-        """Give death of the character"""
-        self.is_alive = False
-
+        pass
 
 class Stark(Character):
     """Your docstring for Class"""
     def __init__(self, name, is_alive=True):
         """Init the Character Stark"""
         super().__init__(name, is_alive)
+
+    def die(self):
+        """Give death of the character"""
+        self.is_alive = False
